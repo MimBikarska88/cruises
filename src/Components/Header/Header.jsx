5bb7bf9f37f6b80d80dda01a8c9d2482ae5a3984
Mimi <mariabikarska2988@gmail.com>
2023-02-05 21:34:44 +0200

    fix footer positioning and add new logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,38 @@
-import logo from '../../assets/img/logo.png';
-import Navigation from './Navigation';
+import logo from "../../assets/img/logo-IO-BAS-small.png";
+import Navigation from "./Navigation";
+import styles from "./Header.module.css";
 const Header = () => {
-    return 		(
+  return (
     <div className="banner-area default-header" id="home">
-    <nav className="navbar navbar-expand-lg  navbar-light">
+      <nav className="navbar navbar-expand-lg  navbar-light">
         <div className="container">
-              <a className="navbar-brand" href="index.html">
-                  <img src={logo}/>
-              </a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="text-white lnr lnr-menu"></span>
-              </button>
-             <Navigation/>
-              						
+          <a className="navbar-brand d-inline-flex" href="index.html">
+            <img src={logo} alt="io-bas-logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="text-white lnr lnr-menu"></span>
+          </button>
+          <div className="d-grid col">
+            <div className={`d-grid row ${styles["row"]}`}>
+              Institute of Oceanology "Fridtjof Nansen"
+            </div>
+            <div className={`d-grid row ${styles["row"]}`}>
+              Bulgarian Academy of Science
+            </div>
+          </div>
+
+          <Navigation />
         </div>
-    </nav>
-</div>
-);
-}
+      </nav>
+    </div>
+  );
+};
 export default Header;
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,21 +1,21 @@
 import Header from "../Header/Header";
 import Cruises from "../Cruises/Cruises";
-import Footer from '../Footer/Footer';
+import Footer from "../Footer/Footer";
 const Layout = () => {
-    return (
-        <>
-        <header>
-            <Header/>
-        </header>
-        
-        <main>
-           <Cruises/>
-        </main>
-        
-        <footer>
-            <Footer/>
-        </footer>
-        </> 
-    );
-}
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+
+      <main>
+        <Cruises />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
 export default Layout;
