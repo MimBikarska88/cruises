bece64a97795923e12b9ff14862210df462bdf6e
Mimi <mariabikarska2988@gmail.com>
2023-02-21 13:55:42 +0200

    refactor general information - cruise page and modify cruise form values

--- src/Components/CruiseForm/GeneralInformation/Cruise/Cruise.jsx
+++ src/Components/CruiseForm/GeneralInformation/Cruise/Cruise.jsx
@@ -2,191 +2,287 @@
  * External Dependencies.
  */
 import { Controller, useFormContext } from "react-hook-form";
+import { useState, useEffect } from "react";
 
 /**
  * Internal Dependencies.
  */
 import styles from "../../CruiseForm.module.css";
 import TextInput from "../../../BaseComponents/TextInput/TextInput";
-import Select from "../../../BaseComponents/Select/Select";
+import Select from "react-select";
 import TextArea from "../../../BaseComponents/TextArea/TextArea";
+import StaticDataService from "../../../../api/StaticDataService";
+import Spinner from "../../../BaseComponents/Spinner/Spinner";
 
 const Cruise = () => {
   const form = useFormContext();
 
+  const service = StaticDataService();
+
+  const [countries, setCountries] = useState([]);
+
+  const [selectedCountryOfDeparture, setSelectedCountryOfDeparture] = useState({
+    id: null,
+    code: null,
+    name: null,
+    label: null,
+  });
+
+  const [selectedCountryOfReturn, setSelectedCountryOfReturn] = useState({
+    id: null,
+    code: null,
+    name: null,
+    label: null,
+  });
+
+  const [selectedPortOfDeparture, setSelectedPortOfDeparture] = useState({
+    id: null,
+    code: null,
+    name: null,
+    label: null,
+  });
+
+  const [selectedPortOfReturn, setSelectedPortOfReturn] = useState({
+    id: null,
+    code: null,
+    name: null,
+    label: null,
+  });
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const mapToSelectOptions = (data) => {
+    return data.map((d) => {
+      return {
+        value: d,
+        label: d.label,
+      };
+    });
+  };
+
+  useEffect(() => {
+    service
+      .loadAllCountries()
+      .then((res) => {
+        setCountries(Object.values(res.data)[0]);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    form.setValue(
+      "general.cruise.countryOfDeparture",
+      selectedCountryOfDeparture
+    );
+  }, [selectedCountryOfDeparture]);
+
+  useEffect(() => {
+    form.setValue("general.cruise.countryOfReturn", selectedCountryOfReturn);
+  }, [selectedCountryOfReturn]);
+
   return (
     <>
-      <div className={`container ${styles["cruise-info"]}`}>
-        <div className="row mb-5">
-          <h4 className="mt-3 mb-1 text-left">Period</h4>
-
-          <div className="col">
-            <Controller
-              control={form.control}
-              name="general.cruise.startDate"
-              render={({ field }) => (
-                <TextInput
-                  type="datetime-local"
-                  label="Start Date"
-                  {...field}
-                />
-              )}
-            />
+      {isLoading ? (
+        <>
+          <div className="d-block m-auto w-100 h-100">
+            <Spinner />
           </div>
+        </>
+      ) : (
+        <div className={`container ${styles["cruise-info"]}`}>
+          <div className="row mb-5">
+            <h4 className="mt-3 mb-1 text-left">Period</h4>
 
-          <div className="col">
-            <Controller
-              control={form.control}
-              name="general.cruise.endDate"
-              render={({ field }) => (
-                <TextInput type="datetime-local" label="End Date" {...field} />
-              )}
-            />
-          </div>
-        </div>
-        <div className="row mb-5">
-          <div className="col">
-            <Select name="countries" title="Country of Departure" />
-
-            <div className="d-flex pt-1">
-              <div className="mr-2">
-                <Controller
-                  control={form.control}
-                  name="general.cruise.countryName"
-                  render={({ field }) => (
-                    <TextInput label="Country" disabled {...field} />
-                  )}
-                />
-              </div>
-
-              <div className="mr-2">
-                <Controller
-                  control={form.control}
-                  name="general.cruise.countryCode"
-                  render={({ field }) => (
-                    <TextInput label="Code" disabled {...field} />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <Select name="countries" title="Port of Departure" />
-
-            <div className="d-flex pt-1">
-              <div className="mr-2">
-                <Controller
-                  control={form.control}
-                  name="general.cruise.portName"
-                  render={({ field }) => (
-                    <TextInput label="Port" disabled {...field} />
-                  )}
-                />
-              </div>
-
-              <div>
-                <Controller
-                  control={form.control}
-                  name="general.cruise.portCode"
-                  render={({ field }) => (
-                    <TextInput label="Code" disabled {...field} />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mb-5">
-          <div className="col">
-            <Select name="countries" title="Country of Return" />
-
-            <div className="d-flex pt-1">
-              <div className="mr-2">
-                <Controller
-                  control={form.control}
-                  name="general.cruise.returnCountryName"
-                  render={({ field }) => (
-                    <TextInput label="Country" disabled {...field} />
-                  )}
-                />
-              </div>
-
-              <div>
-                <Controller
-                  control={form.control}
-                  name="general.cruise.returnCountryCode"
-                  render={({ field }) => (
-                    <TextInput label="Code" disabled {...field} />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <Select name="ports" title="Port of Return" />
-
-            <div className="d-flex pt-1">
-              <div className="mr-2">
-                <Controller
-                  control={form.control}
-                  name="general.cruise.returnPortName"
-                  render={({ field }) => (
-                    <TextInput label="Port" disabled {...field} />
-                  )}
-                />
-              </div>
-
-              <div>
-                <Controller
-                  control={form.control}
-                  name="general.cruise.returnPortCode"
-                  render={({ field }) => (
-                    <TextInput label="Code" disabled {...field} />
-                  )}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row mb-5">
-          <div className="col">
-            <Select name="countries" title="Data Access Restriction *" />
-          </div>
-
-          <div className="col">
-            <div className="mt-10 pt-2">
+            <div className="col">
               <Controller
                 control={form.control}
-                name="general.cruise.dataAccessRestrictionCode"
+                name="general.cruise.startDate"
                 render={({ field }) => (
-                  <TextInput label="Code" disabled {...field} />
+                  <TextInput
+                    type="datetime-local"
+                    label="Start Date"
+                    {...field}
+                  />
                 )}
               />
             </div>
 
-            <div className="mt-10">
+            <div className="col">
               <Controller
                 control={form.control}
-                name="general.cruise.dataAccessRestrictionName"
+                name="general.cruise.endDate"
                 render={({ field }) => (
-                  <TextInput label="Name" disabled {...field} />
+                  <TextInput
+                    type="datetime-local"
+                    label="End Date"
+                    {...field}
+                  />
                 )}
               />
             </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col">
+              <label>Country of Departure</label>
+              <Select
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#007bffed",
+                    primary: "black",
+                  },
+                })}
+                options={mapToSelectOptions(countries)}
+                onChange={(e) => setSelectedCountryOfDeparture(e.value)}
+              />
 
-            <Controller
-              control={form.control}
-              name="general.cruise.dataAccessRestrictionDefinition"
-              render={({ field }) => (
-                <TextArea label="Definition" disabled {...field} />
-              )}
-            />
+              <div className="d-flex pt-1">
+                <div className="mr-2">
+                  <TextInput
+                    label="Country"
+                    disabled
+                    value={selectedCountryOfDeparture.label}
+                  />
+                </div>
+
+                <div className="mr-2">
+                  <TextInput
+                    label="Code"
+                    disabled
+                    value={selectedCountryOfDeparture.code}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <label>Port of Departure</label>
+              <Select
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#007bffed",
+                    primary: "black",
+                  },
+                })}
+              />
+
+              <div className="d-flex pt-1">
+                <div className="mr-2">
+                  <TextInput label="Port" disabled />
+                </div>
+
+                <div>
+                  <TextInput label="Code" disabled />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col">
+              <label>Country of Return</label>
+              <Select
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#007bffed",
+                    primary: "black",
+                  },
+                })}
+                options={mapToSelectOptions(countries)}
+                onChange={(e) => setSelectedCountryOfReturn(e.value)}
+              />
+
+              <div className="d-flex pt-1">
+                <div className="mr-2">
+                  <TextInput
+                    label="Country"
+                    disabled
+                    value={selectedCountryOfReturn.label}
+                  />
+                </div>
+
+                <div>
+                  <TextInput
+                    label="Code"
+                    disabled
+                    value={selectedCountryOfReturn.code}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <label>Port of Return</label>
+              <Select
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#007bffed",
+                    primary: "black",
+                  },
+                })}
+              />
+
+              <div className="d-flex pt-1">
+                <div className="mr-2">
+                  <TextInput
+                    label="Port"
+                    disabled
+                    value={selectedPortOfReturn.label}
+                  />
+                </div>
+
+                <div>
+                  <TextInput
+                    label="Code"
+                    disabled
+                    value={selectedPortOfReturn.code}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row mb-5">
+            <div className="col pt-2">
+              <label>Data Access Restriction</label>
+              <Select
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#007bffed",
+                    primary: "black",
+                  },
+                })}
+              />
+            </div>
+
+            <div className="col">
+              <div className="mt-10">
+                <TextInput label="Code" disabled />
+              </div>
+
+              <div className="mt-10">
+                <TextInput label="Name" disabled />
+              </div>
+
+              <TextArea label="Definition" disabled />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
--- src/api/StaticDataService.jsx
+++ src/api/StaticDataService.jsx
@@ -11,11 +11,14 @@ const StaticDataService = () => {
 
   const getBioIndicators = () => axios.get(`${path}/bio-indicators`);
 
+  const getCountries = () => axios.get(`${path}/countries`);
+
   return {
     loadAllSeaScapeParameters: getSeaScapeParameters,
     loadAllInstruments: getInstruments,
     loadAllPlatformCategories: getPlatformCategories,
     loadAllBioIndicators: getBioIndicators,
+    loadAllCountries: getCountries,
   };
 };
 export default StaticDataService;
